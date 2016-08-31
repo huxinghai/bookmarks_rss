52812bd1385054396c0344ad4e3dfbf73caf6b72
kaka <huxinghai1988@gmail.com>
2016-08-31 14:45:02 +0800

    site info

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -6,28 +6,11 @@ class Api::V1::BookmarksController < ApplicationController
   def create
     data = params.require(:bookmarks)
     data.each do |param|
-      options = param.permit(:provision_id, :parent_id, :index, :url, :title, :date_added, :date_group_modified)
-      begin
-        url = FeedCrawler.fetch(options[:url]) 
-        if url.present?
-          res = FeedParser.new(url: url).parse.as_json
-          site_info = current_user.site_infos.find_or_initialize_by(url: res[:url])
-          site_info.title = res[:title]
-          site_info.save!
-
-          res[:items].each do |article|
-            Rails.logger.info("url: #{site_info.url}, user: #{current_user.id}, article: #{article}")
-            article = site_info.articles.find_or_initialize_by(title: article[:title])
-            article.assign_attributes(article.slice(:link, :published, :author, :description, :content, :guid))
-            article.save!
-          end
-        end
-        head :ok
-      rescue Exception => e
-        render json: {errors: "记录书签信息失败！"}, status: :unprocessable_entity  
-      end
+      options = param.permit(:url) #:title, :date_added, :date_group_modified, :provision_id, :parent_id, :index
+      SiteInfo.crawler_articles(options[:url])
     end
-
+    
+    head :ok
   end
 
   def destroy
--- app/models/site_info.rb
+++ app/models/site_info.rb
@@ -5,4 +5,31 @@ class SiteInfo < ApplicationRecord
 
   validates :user, presence: true
 
+  class << self
+    def crawler_articles(url)
+      return if url.blank? || url =~ URI::regexp(['http', 'https'])
+      res = crawler_feed(FeedCrawler.fetch(options[:url]))
+      if res.present?
+        site_info = self.find_or_initialize_by(url: res[:url])
+        site_info.title = res[:title]
+        site_info.save!
+        res[:items].map do |article|
+          Rails.logger.info("url: #{site_info.url}, article: #{article}")
+          article = site_info.articles.find_or_initialize_by(title: article[:title])
+          article.assign_attributes(article.slice(:link, :published, :author, :description, :content, :guid))
+          article.save!
+          article
+        end
+      end
+    end
+
+    def crawler_feed(rss_url)
+      begin
+        rss_url.present? ? FeedParser.new(url: rss_url).parse.as_json : {}
+      rescue Exception => e
+        Rails.logger.error("url: #{rss_url}, error: #{e.message}")
+        {}
+      end
+    end
+  end
 end
