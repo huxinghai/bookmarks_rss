90777fa01ee37b9c358e9a6a1bd3728eee6eecd2
kaka <huxinghai1988@gmail.com>
2016-09-03 20:09:53 +0800

    fixed migrate article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,7 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = current_user.articles.page(params[:page]).per(25)
+    @articles = @articles.where(site_info_id: params[:site_id]) if params[:site_id].present?
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
 
   def show
-    @articles = current_user.articles.page(params[:page]).per(25)
-    @articles = @articles.where(site_info_id: params[:site_id]) if params[:site_id].present?
   end
 
 end
