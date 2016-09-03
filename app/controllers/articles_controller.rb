ea66b2b4b62e03b5632f5173d867813e3b851b02
kaka <huxinghai1988@gmail.com>
2016-09-03 22:36:09 +0800

    include querys

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = current_user.articles.page(params[:page]).per(25)
+    @articles = current_user.articles.includes(:site_info).page(params[:page]).per(25)
     @articles = @articles.where(site_info_id: params[:site_id]) if params[:site_id].present?
   end
 end
